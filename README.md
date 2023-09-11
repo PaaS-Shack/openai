53222012b3bbe09fa65dce350c11f90762bac8ea
FLYBYME <mangoraft@gmail.com>
2023-09-11 14:40:38 -0400

    Update

--- README.md
+++ README.md
@@ -1,35 +1,156 @@
-[![Moleculer](https://badgen.net/badge/Powered%20by/Moleculer/0e83cd)](https://moleculer.services)
+# Moleculer OpenAI Service
 
-# kube
-This is a [Moleculer](https://moleculer.services/)-based microservices project. Generated with the [Moleculer CLI](https://moleculer.services/docs/0.14/moleculer-cli.html).
+![Moleculer logo](https://moleculer.services/images/logo.png)
+
+This is a Moleculer service for integrating with OpenAI's GPT models. It provides REST API endpoints for various natural language processing tasks such as text completion, search, and classification using the OpenAI API.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [Actions](#actions)
+- [Examples](#examples)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone this repository:
+
+```bash
+git clone https://github.com/Paas-Shack/openai.git
+cd openai
+```
+
+2. Install dependencies:
+
+```bash
+npm install
+```
+
+3. Configure the service by editing the `moleculer.config.js` file and providing your OpenAI API key and organization.
+
+4. Start the service:
+
+```bash
+npm start
+```
+
+## Configuration
+
+You need to configure the service with your OpenAI API key and organization. Edit the `moleculer.config.js` file and provide the following configuration in the `nodeID` section:
+
+```javascript
+// moleculer.config.js
+
+module.exports = {
+  // ...
+  nodeID: "your-node-id",
+  transporter: "NATS",
+  // ...
+  services: [
+    {
+      name: "openai",
+      // ...
+      settings: {
+        config: {
+          "openai.org": "your-organization-id",
+          "openai.key": "your-api-key"
+        }
+      }
+    }
+  ]
+  // ...
+};
+```
 
 ## Usage
-Start the project with `npm run dev` command. 
-In the terminal, try the following commands:
-- `nodes` - List all connected nodes.
-- `actions` - List all registered service actions.
-- `call greeter.hello` - Call the `greeter.hello` action.
-- `call greeter.welcome --name John` - Call the `greeter.welcome` action with the `name` parameter.
 
+You can use this service by making HTTP requests to its REST API endpoints. Here are the available actions:
 
+### Create Completion
 
-## Services
-- **api**: API Gateway services
-- **greeter**: Sample service with `hello` and `welcome` actions.
+Generate text completions based on a prompt:
 
+```http
+POST /v1/openai/completion
+```
 
-## Useful links
+**Parameters:**
 
-* Moleculer website: https://moleculer.services/
-* Moleculer Documentation: https://moleculer.services/docs/0.14/
+- `prompt` (string, required): The text prompt.
+- `engine` (string, optional): The OpenAI engine to use (default: "davinci").
+- `maxTokens` (number, optional): Maximum number of tokens in the response (default: 100).
+- `temperature` (number, optional): Sampling temperature (default: 0.5).
 
-## NPM scripts
+### Create Search
 
-- `npm run dev`: Start development mode (load all services locally with hot-reload & REPL)
-- `npm run start`: Start production mode (set `SERVICES` env variable to load certain services)
-- `npm run cli`: Start a CLI and connect to production. Don't forget to set production namespace with `--ns` argument in script
-- `npm run lint`: Run ESLint
-- `npm run ci`: Run continuous test mode with watching
-- `npm test`: Run tests & generate coverage report
-- `npm run dc:up`: Start the stack with Docker Compose
-- `npm run dc:down`: Stop the stack with Docker Compose
+Perform a search operation on a set of documents using a query:
+
+```http
+POST /v1/openai/search
+```
+
+**Parameters:**
+
+- `documents` (string, required): The set of documents.
+- `query` (string, required): The search query.
+
+### Create Classification
+
+Perform a text classification task with given examples and labels:
+
+```http
+POST /v1/openai/classification
+```
+
+**Parameters:**
+
+- `query` (string, required): The text query.
+- `examples` (string, required): Examples for classification.
+- `labels` (string, required): Labels for classification.
+
+## Examples
+
+Here are some examples of how to use the service:
+
+### Create Completion
+
+```http
+POST /v1/openai/completion
+{
+  "prompt": "Translate the following English text to French: 'Hello, world.'"
+}
+```
+
+### Create Search
+
+```http
+POST /v1/openai/search
+{
+  "documents": "Document 1\nDocument 2\nDocument 3",
+  "query": "Search query"
+}
+```
+
+### Create Classification
+
+```http
+POST /v1/openai/classification
+{
+  "query": "Is this a positive or negative statement?",
+  "examples": "Example 1\nExample 2\nExample 3",
+  "labels": "positive\nnegative\nneutral"
+}
+```
+
+For more details on the available actions and their parameters, refer to the [Actions](#actions) section.
+
+## Contributing
+
+Contributions are welcome! Please follow the [Contribution Guidelines](CONTRIBUTING.md) for this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- services/openai.service.js
+++ services/openai.service.js
@@ -11,7 +11,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 
 /**
- * attachments of addons service
+ * OpenAI Service
  */
 module.exports = {
     name: "openai",
@@ -34,6 +34,11 @@ module.exports = {
     settings: {
         rest: "/v1/openai/",
 
+        config: {
+            "openai.org": "org-...",//process.env.OPENAI_ORG,
+            "openai.key": "key-..."//process.env.OPENAI_KEY
+
+        }
     },
 
     /**
@@ -41,31 +46,131 @@ module.exports = {
      */
 
     actions: {
-
-        listEngines: {
-            rest: 'GET /list-engines',
-            params: {
-
-            },
-            async handler(ctx) {
-                const params = Object.assign({}, ctx.params);
-                return this.openai.listEngines()
-            }
-        },
+        /**
+         * create a completion
+         * 
+         * @actions
+         * @param {string} prompt
+         * @param {string} engine
+         * @param {number} maxTokens
+         * @param {number} temperature
+         * 
+         * @returns {Promise} completion - completion object
+         */
         createCompletion: {
+            rest: {
+                method: "POST",
+                path: "/completion"
+            },
+            permissions: ["openai.createCompletion"],
             params: {
-                prompt: { type: "string", optional: false },
+                prompt: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                },
+                engine: {
+                    type: "string",
+                    enum: ["ada", "babbage", "curie", "davinci", "content-filter-alpha-c4", "content-filter-dev"],
+                    default: "davinci"
+                },
+                maxTokens: {
+                    type: "number",
+                    min: 1,
+                    max: 1000,
+                    default: 100
+                },
+                temperature: {
+                    type: "number",
+                    min: 0,
+                    max: 1,
+                    default: 0.5
+                }
             },
             async handler(ctx) {
-                const params = Object.assign({}, ctx.params);
-                return this.openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: params.prompt,
-                    max_tokens: 7,
-                    temperature: 0,
+                const { prompt, engine, maxTokens, temperature } = ctx.params;
+                const completion = await this.createCompletion(prompt, engine, maxTokens, temperature).catch((err) => {
+                    console.log(err)
+                    throw new MoleculerClientError("OpenAI API error", 500, "OPENAI_API_ERROR", { code: 500, message: "OpenAI API error" });
                 });
+                return completion;
             }
         },
+
+        /**
+         * create a search
+         * 
+         * @actions
+         * @param {string} documents
+         * @param {string} query
+         * 
+         * @returns {Promise} search - search object
+         */
+        createSearch: {
+            rest: {
+                method: "POST",
+                path: "/search"
+            },
+            permissions: ["openai.createSearch"],
+            params: {
+                documents: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                },
+                query: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                }
+            },
+            async handler(ctx) {
+                const { documents, query } = ctx.params;
+                const search = await this.createSearch(documents, query);
+                return search;
+            }
+        },
+
+        /**
+         * create a classification
+         * 
+         * @actions
+         * @param {string} query
+         * @param {string} examples
+         * @param {string} labels
+         * 
+         * @returns {Promise} classification - classification object
+         */
+        createClassification: {
+            rest: {
+                method: "POST",
+                path: "/classification"
+            },
+            permissions: ["openai.createClassification"],
+            params: {
+                query: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                },
+                examples: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                },
+                labels: {
+                    type: "string",
+                    min: 1,
+                    max: 1000
+                }
+            },
+            async handler(ctx) {
+                const { query, examples, labels } = ctx.params;
+                const classification = await this.createClassification(query, examples, labels);
+                return classification;
+            }
+        },
+
     },
 
     /**
@@ -79,6 +184,94 @@ module.exports = {
      * Methods
      */
     methods: {
+        /**
+         * create a completion
+         * 
+         * @param {string} prompt
+         * @param {string} engine
+         * @param {number} maxTokens
+         * @param {number} temperature
+         * 
+         * @returns {Promise} completion - completion object
+         */
+        async createCompletion(prompt, engine, maxTokens, temperature) {
+            const completion = await this.openai.createCompletion({
+                engine: engine,
+                prompt: prompt,
+                maxTokens: maxTokens,
+                temperature: temperature,
+                topP: 1,
+                frequencyPenalty: 0,
+                presencePenalty: 0,
+                stop: ["\n"]
+            });
+            return completion;
+        },
+
+        /**
+         * create a search
+         * 
+         * @param {string} documents
+         * @param {string} query
+         * 
+         * @returns {Promise} search - search object
+         */
+        async createSearch(documents, query) {
+            const search = await this.openai.createSearch({
+                documents: documents,
+                query: query
+            });
+            return search;
+        },
+
+        /**
+         * create a classification
+         * 
+         * @param {string} query
+         * @param {string} examples
+         * @param {string} labels
+         * 
+         * @returns {Promise} classification - classification object
+         */
+        async createClassification(query, examples, labels) {
+            const classification = await this.openai.createClassification({
+                query: query,
+                examples: examples,
+                labels: labels
+            });
+            return classification;
+        },
+
+        /**
+         * create a answer
+         * 
+         * @param {string} documents
+         * @param {string} question
+         * 
+         * @returns {Promise} answer - answer object
+         */
+        async createAnswer(documents, question) {
+            const answer = await this.openai.createAnswer({
+                documents: documents,
+                question: question
+            });
+            this.logger.info(`Prompt: ${question} Answer: ${answer}`);
+            return answer;
+        },
+
+
+        /**
+         * create openai client
+         */
+        createOpenAiClient() {
+            this.configuration = new Configuration({
+                apiKey: this.config["openai.key"],
+            });
+
+            this.openai = new OpenAIApi(this.configuration);
+            
+            this.logger.info("OpenAI client created");
+        },
 
     },
 
@@ -92,12 +285,8 @@ module.exports = {
     /**
      * Service started lifecycle event handler
      */
-    started() {
-        this.configuration = new Configuration({
-            organization: this.config['openai.org'],
-            apiKey: this.config['openai.key'],
-        });
-        this.openai = new OpenAIApi(this.configuration);
+    async started() {
+        await this.createOpenAiClient();
     },
 
 
